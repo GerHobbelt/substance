ed52eefe9da9f682837f7c4a96505a83c50037dc
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-09-20 13:55:58 +0200

    Added test for insertNode trafo.

---
+++ test/fixtures/simple.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Article = require('../test_article');
+
+module.exports = function() {
+  var article = new Article();
+  article.set(['meta', 'title'], 'Sample1');
+  article.create({
+    type: 'paragraph',
+    id: 'p1',
+    content: '0123456789'
+  });
+  article.create({
+    type: 'paragraph',
+    id: 'p2',
+    content: '0123456789'
+  });
+  article.create({
+    type: 'paragraph',
+    id: 'p3',
+    content: '0123456789'
+  });
+  article.create({
+    type: 'paragraph',
+    id: 'p4',
+    content: '0123456789'
+  });
+  var main = article.get('main');
+  main.show('p1');
+  main.show('p2');
+  main.show('p3');
+  main.show('p4');
+  article.documentDidLoad();
+  article.FORCE_TRANSACTIONS = false;
+  return article;
+};
